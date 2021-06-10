7031813be6fb5237b4b6c49c9ef2bba14d38c66a
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-06-10 20:42:06 +0400

    first commit

---
+++ src/main/java/by/akimova/TP_lab_servlet/FirstServlet.java
@@ -0,0 +1,101 @@
+package by.akimova.TP_lab_servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("src/webapp/index.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        double result = 0;
+        String error = "";
+
+    //    String messageResult;                                   // строка результата
+        String num1 = request.getParameter("num1"); //получаем первое число для операции
+        String num2 = request.getParameter("num2"); //получаем второе число для операции
+
+        if (!num1.isEmpty() && !num2.isEmpty()) {  // проверяем пришедшие значения на наличие
+            double a1 = Double.parseDouble(num1);  // конвертируем строку в double
+            double a2 = Double.parseDouble(num2);
+
+            try {
+            char parameters = request.getParameter("parameters").charAt(0); // получаем map по input c формы, в котором name - ключ, value - значение
+            switch(parameters) {
+                case '+':
+                    result =  a1 + a2;
+                    break;
+                case '-':
+                    result =  a1 - a2;
+                    break;
+                case '*':
+                    result =  a1 * a2;
+                    break;
+                case '/':
+                    result =  a1 / a2;
+                    break;
+            }
+        }
+		catch(ArithmeticException e) {
+            error = "La division par zéro est interdite";
+        }
+		catch(Exception e) {
+            error = "Vous n'avez probablement  pas effectué les opérations sur des entiers";
+        }
+
+        request.setAttribute("result", Double.toString(result));
+        request.setAttribute("error", error);
+        this.doGet(request, response);
+    }
+
+
+
+//            if (parameters.containsKey("+")) {           // проверяем какое действие было выполнено
+//                result = a1+a2;
+//            } else if (parameters.containsKey("-")) {
+//                result = a1-a2;
+//            } else if (parameters.containsKey("*")) {
+//                result = a1*a2;
+//            } else if (parameters.containsKey("/")) {
+//                result = a1/a2;
+//            }
+//            messageResult = String.valueOf((double) result);            // если все ок, конвертируем результат операции в строку и присваиваем messageResult
+//        } else {
+//            messageResult = "Одно из чисел не было введено. Пожалуйста, попробуйте снова."; // если не введено одно из чисел для вычисления, в messageResult записывается текст ошибки
+//        }
+//        response(response, messageResult); // запускаем метод response для вывода формы с результатом
+
+
+
+//            switch (command) {
+//            case "+":
+//                result = num1 + num2;
+//                operation = "+";
+//                break;
+//            case "-":
+//                result = num1 - num2;
+//                operation = "-";
+//                break;
+//            case "*":
+//                result = num1 * num2;
+//                operation = "*";
+//                break;
+//            case "/":
+//                result = num1 / num2;
+//                operation = "/";
+//                break;
+//        }
+//        String action = String.format("%d %s %d = %d", num1, operation, num2, result);
+
+        }
+    }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,63 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Calculate</title>
+</head>
+<body>
+<h1><%= "Вариант 1" %>
+</h1>
+<h1>
+    <div>Welcome to Calculator App</div>
+</h1>
+<br/>
+<form method="post" action="/FirstServlet">
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+            <input name="num1" type="text" class="form-control" aria-label="Sizing example input"
+                   aria-describedby="inputGroup-sizing-lg" placeholder="Enter first number ">
+        </div>
+        <div class="col">
+            <select name="parameters" class="form-select form-select-lg mb-1" aria-label=".form-select-lg example">
+                <option selected>Select option</option>
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+        </div>
+        <div class="col">
+            <input name="num2" type="text" class="form-control" aria-label="Sizing example input"
+                   aria-describedby="inputGroup-sizing-lg" placeholder="Enter second number ">
+        </div>
+    </div>
+</div>
+<div>
+    <button value="calculate" type="submit" class="btn btn-primary btn-lg">Calculate</button>
+</div>
+<div>
+    <input  name="result" class="form-control" type="text" placeholder="Answer here " aria-label="readonly input example" readonly>
+</div>
+
+    <p>Le résultat est : ${!empty(error)?err:result}</p>
+
+</form>
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
+        integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT"
+        crossorigin="anonymous"></script>
+
+</html>
